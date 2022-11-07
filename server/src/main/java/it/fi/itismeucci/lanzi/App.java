3ef25abc1537db41cb39d51c879da8aab283e6e3
F3kFak <ary.maggiorelli@gmail.com>
2022-11-07 10:14:18 +0100

    inizio

---
+++ server/src/main/java/it/fi/itismeucci/lanzi/App.java
@@ -0,0 +1,13 @@
+package it.fi.itismeucci.lanzi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
